688360a942f4a35365c619087b9046c3eea40424
kp_pfc <krittayos.p@perfectcomsolutions.com>
2025-02-16 21:33:45 +0700

    update: list title

--- src/variables/showcase/showcase-list.ts
+++ src/variables/showcase/showcase-list.ts
@@ -1,18 +1,5 @@
 export const showcaseList = [
    {
-      title: 'นักเรียนรุ่นแรกฝึกลอกจอนักเรียนรุ่นแรกฝึกลอกจอนักเรียนรุ่นแรกฝึกลอกจอนักเรียนรุ่นแรกฝึกลอกจอ',
-      date: '2024-12-01',
-      thumbnailSrc: '/images/showcase/1/2.png',
-      videoSrc: '/video/showcase/1/mock.mp4',
-      imgSrc: [
-         '/images/showcase/1/1.png',
-         '/images/showcase/1/2.png',
-         '/images/showcase/1/3.png',
-      ],
-      detail:
-         'Lorem ipsum dolor sit amet consectetur. Auctor lectus imperdiet commodo pretium. Mattis in habitant odio malesuada dis tristique. Amet amet vel justo viverra sit eu leo ac. Tristique nec velit rutrum montes sit posuere ac. Est elementum non pellentesque a fringilla senectus quis ut gravida. Sed pellentesque lorem nisl scelerisque aliquet tellus volutpat arcu. Pretium aenean felis felis at dolor quis id turpis. Velit in risus id tellus sagittis facilisis. Convallis eget sagittis sit senectus eget congue.',
-   },
-   {
       title: 'นักเรียนรุ่นแรกฝึกลอกจอ',
       date: '2024-12-01',
       thumbnailSrc: '/images/showcase/1/2.png',
@@ -26,7 +13,7 @@ export const showcaseList = [
          'Lorem ipsum dolor sit amet consectetur. Auctor lectus imperdiet commodo pretium. Mattis in habitant odio malesuada dis tristique. Amet amet vel justo viverra sit eu leo ac. Tristique nec velit rutrum montes sit posuere ac. Est elementum non pellentesque a fringilla senectus quis ut gravida. Sed pellentesque lorem nisl scelerisque aliquet tellus volutpat arcu. Pretium aenean felis felis at dolor quis id turpis. Velit in risus id tellus sagittis facilisis. Convallis eget sagittis sit senectus eget congue.',
    },
    {
-      title: 'นักเรียนรุ่นแรกฝึกลอกจอ',
+      title: 'นักเรียนรุ่นสองฝึกลอกจอ',
       date: '2024-12-01',
       thumbnailSrc: '/images/showcase/1/2.png',
       videoSrc: '/video/showcase/1/mock.mp4',
@@ -39,7 +26,20 @@ export const showcaseList = [
          'Lorem ipsum dolor sit amet consectetur. Auctor lectus imperdiet commodo pretium. Mattis in habitant odio malesuada dis tristique. Amet amet vel justo viverra sit eu leo ac. Tristique nec velit rutrum montes sit posuere ac. Est elementum non pellentesque a fringilla senectus quis ut gravida. Sed pellentesque lorem nisl scelerisque aliquet tellus volutpat arcu. Pretium aenean felis felis at dolor quis id turpis. Velit in risus id tellus sagittis facilisis. Convallis eget sagittis sit senectus eget congue.',
    },
    {
-      title: 'นักเรียนรุ่นแรกฝึกลอกจอ',
+      title: 'นักเรียนรุ่นสามฝึกลอกจอ',
+      date: '2024-12-01',
+      thumbnailSrc: '/images/showcase/1/2.png',
+      videoSrc: '/video/showcase/1/mock.mp4',
+      imgSrc: [
+         '/images/showcase/1/1.png',
+         '/images/showcase/1/2.png',
+         '/images/showcase/1/3.png',
+      ],
+      detail:
+         'Lorem ipsum dolor sit amet consectetur. Auctor lectus imperdiet commodo pretium. Mattis in habitant odio malesuada dis tristique. Amet amet vel justo viverra sit eu leo ac. Tristique nec velit rutrum montes sit posuere ac. Est elementum non pellentesque a fringilla senectus quis ut gravida. Sed pellentesque lorem nisl scelerisque aliquet tellus volutpat arcu. Pretium aenean felis felis at dolor quis id turpis. Velit in risus id tellus sagittis facilisis. Convallis eget sagittis sit senectus eget congue.',
+   },
+   {
+      title: 'นักเรียนรุ่นสี่ฝึกลอกจอ',
       date: '2024-12-01',
       thumbnailSrc: '/images/showcase/1/2.png',
       videoSrc: '/video/showcase/1/mock.mp4',
