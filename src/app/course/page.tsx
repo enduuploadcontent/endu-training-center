9140fbc63868248b0b8d62afb5b47d62bd5b93a6
khawpunkp <krittayos.kp@gmail.com>
2025-04-29 19:57:24 +0700

    update: add course search

--- src/app/course/page.tsx
+++ src/app/course/page.tsx
@@ -3,9 +3,23 @@
 import CourseCard from '@/components/course/courseCard';
 import Notfound from '@/components/ui/notfound';
 import CustomTypography from '@/components/ui/typography';
+import { nonCaseSensitiveSearch } from '@/utils/misc';
 import { courseList } from '@/variables/course/course';
+import { Input } from 'antd';
+import { useState } from 'react';
 
 export default function CoursePage() {
+   const [filteredList, setFilteredList] = useState(courseList);
+
+   const [searchInput, setSearchInput] = useState<string>('');
+
+   const search = () => {
+      const filteredList = courseList.filter((c) =>
+         nonCaseSensitiveSearch(c.title, searchInput),
+      );
+      setFilteredList(filteredList);
+   };
+
    return (
       <div className='flex flex-col gap-4 min-h-[calc(100vh-190px)] mobile:min-h-[calc(100vh-250px)] w-full items-center justify-between pt-28 mobile:pt-20 pb-8 mobile:p-6'>
          <div className='flex flex-col gap-4 w-full max-w-6xl flex-grow'>
@@ -19,13 +33,22 @@ export default function CoursePage() {
                   className='text-foreground-secondary'
                >{`ทั้งหมด ${courseList.length} รายการ`}</CustomTypography>
             </div>
-            {courseList.length === 0 ? (
+            <Input.Search
+               size='large'
+               placeholder='ค้นหา'
+               className='!w-[360px]'
+               value={searchInput}
+               onChange={(e) => setSearchInput(e.target.value)}
+               onPressEnter={search}
+               onSearch={search}
+            />
+            {filteredList.length === 0 ? (
                <div className='h-full w-full flex-grow flex items-center justify-center'>
                   <Notfound />
                </div>
             ) : (
                <div className='grid grid-cols-3 w-full mobile:grid-cols-1 gap-4'>
-                  {courseList.map((content, index) => (
+                  {filteredList.map((content, index) => (
                      <CourseCard key={index} content={content} />
                   ))}
                </div>
