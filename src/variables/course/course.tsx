12ac383bcb6f67540d0ff51705920841b8deb136
khawpunkp <krittayos.kp@gmail.com>
2026-09-09 15:08:45 +0700

    [update] remove skill details from Hardware OCA H1 course

--- src/variables/course/course.tsx
+++ src/variables/course/course.tsx
@@ -136,10 +136,7 @@ export const courseList: CourseContentType[] = [
       level: CourseLevel.Basic,
       title: 'Hardware OCA H1',
       detail: 'ลอกกระจกหน้าจอแบบเรียบและแบบโค้ง',
-      skill: [
-         'ลอกกระจก OCA',
-         'เมื่อซื้อเครื่องมือช่างครบ 50,000 บาท รับสิทธิ์พิเศษ เรียนลอกกระจกฟรี!',
-      ],
+      skill: ['ลอกกระจก OCA'],
       suitableFor: [
          'ผู้ที่มีพื้นฐานการแกะเครื่องเบื้องต้น',
          'ผู้ที่ต้องการเพิ่มรายได้',
